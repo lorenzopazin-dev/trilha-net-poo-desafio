d04dc53db6eb443d10bf3b8ffad3a017e47e0815
Lorenzo Pazin <lorenzopazin.dev@gmail.com>
2024-01-14 19:40:56 -0300

    Desafio Smartphone - Concluído!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "1324", modelo: "Model-1", imei: "11112222", memoria: 64);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "9764", modelo: "Model-5", imei: "44448888", memoria: 256);
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Whatsapp");
